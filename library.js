a7fc58a4a75587c5c2faf10b3e7219ca8d3a5d53
Tales-1 <84051594+Tales-1@users.noreply.github.com>
2022-02-10 21:45:54 +0000

    Update library.js

--- library.js
+++ library.js
@@ -1,68 +1,72 @@
-
-
-
 export const words = [
-    ["K","A","R","E","N"],
-    ["S","I","G","M","A"],
-    ["S","I","M","P","S"],
-    ["S","K","I","N","T"],
-    ["P","A","N","T","S"],
-    ["I","N","N","I","T"],
-    ["J","O","K","E","S"],
-    ["N","O","C","A","P"],
-    ["S","Q","U","A","D"],
-    ["T","I","G","H","T"],
-    ["N","A","T","T","Y"],
-    ["S","K","E","E","N"],
-    ["B","A","S","I","C"],
-    ["G","H","O","S","T"],
-    ["S","A","L","T","Y"],
-    ["S","H","A","D","E"],
-    ["S","H","O","O","K"],
-    ["E","X","T","R","A"],
-    ["G","O","A","T","S"],
-    ["L","I","N","G","O"],
-    ["G","U","C","C","I"],
-    ["C","U","R","V","E"],
+    {
+        word: ["K","A","R","E","N"],
+    },
+    {
+        word:["S","I","G","M","A"],
+    },
+    {
+        word: ["S","I","M","P","S"],
+    },
+    {
+        word:["S","K","I","N","T"],
+    },
+    {
+        word:["P","A","N","T","S"],
+    },
+    {
+        word:["I","N","N","I","T"],
+    },
+    {
+        word:["J","O","K","E","S"],
+    },
+    {
+        word:["N","O","C","A","P"],
+    },
+    {
+        word:["S","Q","U","A","D"],
+    },
+    {
+        word:["T","I","G","H","T"],
+    },
+    {
+        word: ["N","A","T","T","Y"],
+    },
+    {
+        word:["S","K","E","E","N"],
+    },
+    {
+        word: ["B","A","S","I","C"],
+    },
+    {
+        word:["G","H","O","S","T"],
+    },
+    {
+        word:["S","A","L","T","Y"],
+    },
+    {
+        word:["S","H","A","D","E"],
+    },
+    {
+        word:["S","H","O","O","K"],
+    },
+    {
+        word:["E","X","T","R","A"],
+    },
+    {
+        word:["G","O","A","T","S"],
+    },
+    {
+        word:["L","I","N","G","O"],
+    },
+    {
+        word:["G","U","C","C","I"],
+    },
+    {
+        word:["C","U","R","V","E"],
+    },
+
 ]
 
 
-
-export let timeLeft = ()=> {
-    // let tomorrow = new Date().getDate()+1;
-    const timeContainer = document.getElementById("time-el");
-    let metrics = Array.from(timeContainer.children);
-    let tomorrow = new Date().getDate()+1;
-    let thisMonth = new Date().getMonth();
-    let deadline = new Date(2022, thisMonth, tomorrow,0,0).getTime();
-    let currentTime = new Date().getTime();
-    let remainder = deadline - currentTime;
-    const oneDay = 24 * 60 * 60 * 1000;
-    const oneHour = 60 * 60 * 1000;
-    const oneMinute = 60 * 1000;
-    if(remainder < 1){
-        deadline = new Date(2022, thisMonth,tomorrow,0,0).getTime();
-        remainder = deadline-currentTime;
-        removeFromLocalStorage()
-        words.shift();
-        play = true;
-    }
-
-    let hours = Math.floor((remainder % oneDay) / oneHour);
-    let minutes = Math.floor((remainder % oneHour ) / oneMinute);
-    let seconds = Math.floor((remainder % oneMinute) / 1000);
-
-    const addZero = (i)=>{
-        if(i < 10){
-            return `0${i}`
-        }
-        return i
-    }
-
-    let remaining = [hours,minutes,seconds];
-    metrics.forEach((metric,index)=>{
-        metric.innerHTML = addZero(remaining[index]);
-    })
-    
-}
 
