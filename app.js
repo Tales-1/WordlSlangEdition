d79da88c5591bdbfbb4ffd4dac2f1c107ed32206
Tales-1 <84051594+Tales-1@users.noreply.github.com>
2022-02-17 12:04:55 +0000

    Update app.js

--- app.js
+++ app.js
@@ -38,141 +38,14 @@ let counter = 0;
 let targetWord;
 let displayArray = [firstTry,secondTry,thirdTry,fourthTry,fifthTry,sixthTry];
 const definitionSlider = document.querySelector(".definition-slider")
-const definition = document.createElement("p");
-definition.setAttribute("id", "definition");
-definition.innerHTML="Definition";
+const definitionBtn = document.createElement("p");
+definitionBtn.setAttribute("id", "definition");
+definitionBtn.innerHTML="Definition";
 const shareCont = document.querySelector(".share")
 const triangle = document.getElementById("triangle")
 let selectDefinition;
 const debug = document.getElementById("debug")
 
-
-
-
-// generate ID and select word of the day
-
-selectWord(words,targetWord)
-let definitionContent = "";
-const inputTarget = (targetWord)=>{
-    definitionContent = targetWord.reduce((a,b)=>a+b);
-};
-let insertDefinition = document.querySelector(".insert-definition");
-inputTarget(targetWord)
-insertDefinition.innerHTML= definitionContent;
-
-//EVENT LISTENERS
-window.addEventListener("DOMContentLoaded",()=>{
-    let items = getLocalStorage();
-    if(items.length>0){
-        refreshDisplay(items);
-    }
-    
-})
-
-
-
-container.addEventListener("click", e=>{
-  if(!e.target.closest("span")) return;
-
-  if(e.target.closest("span") && play){
-  const targetLetter = e.target.innerHTML;
- inputLetters(targetLetter)
-
-    }
-
-});
-
-backspace.addEventListener("click",()=>{
-    turn[counter-1].innerHTML = "";
-    counter--
-})
-
-enter.addEventListener("click",()=>{
-    let copy = [...targetWord];
-    let answer=[];
-    
-    if(play && counter ===5){
-    turn.forEach((letter,index)=>{
-        answer.push(letter.innerHTML);
-        for(let i = 0; i<targetWord.length;i++){
-            if(letter.innerHTML === targetWord[i] && index === i){
-            letter.classList.remove("orange")
-            setTimeout(changeGreen,270*index,letter)
-            targetWord[i] = "."
-            return;
-           }
-
-           else if(letter.innerHTML === targetWord[i] && index !== i){
-           letter.classList.remove("wrong")
-           setTimeout(changeOrange,270*index,letter)
-           
-           
-           }
-
-        }
-        if(!letter.classList.contains("green") && !letter.classList.contains("orange")){
-            setTimeout(changeGrey,270*index,letter);
-        }
-
-    })
-    targetWord = copy;
-    addToLocalStorage(answer);
-    let checker = 0;
-    for(let i=0; i<targetWord.length;i++){
-        if(answer[i]===targetWord[i]){
-            checker++
-        }
-    }
- 
-    if(checker ===5 || turn ===sixthTry){
-        setTimeout(result,2500);
-        shareBtn.innerHTML = `<a href="whatsapp://send?text=I got ${turnNo}/6 try Wordl Slang! @ https://tales-1.github.io/WordlSlangEdition/">Share Result</a>`;
-        shareCont.appendChild(definition)
-        
-        addEvtListner()
-        play = false;
-        
-    }
-    
-    turnNo++
-    counter = 0;
-}
-else{
-    showAlert("Not enough letters")}
-})
-
-
-closeBtn.addEventListener("click", ()=>{
-    youWin.classList.remove("show")
-})
-
-shareBtn.addEventListener("click",()=>{
-    updateClipboard(`
-    I got ${turnNo}/6, try Wordl!`
-    );
-    showAlert("Copied to clipboard!")
-})
-
-cog.addEventListener("click",()=>{
-    result()
-    console.log(play)
-    if(play===false){
-        shareBtn.innerHTML =`<a href="whatsapp://send?text=I got ${turnNo-1}/6 try Wordl Slang! @ https://tales-1.github.io/WordlSlangEdition/">Share Result</a>`;
-        shareCont.appendChild(definition)
-        addEvtListner()
-
-    }
-
-})
-
-definitionSlider.addEventListener("click",()=>{
-    definitionSlider.classList.remove("slide")
-})
-
-debug.addEventListener("click",()=>{
-    removeFromLocalStorage();
-    window.location = window.location.href;
-})
 
 
 
@@ -259,7 +132,7 @@ function changeGrey(letter){
 }
 
 function addEvtListner(){
-    definition.addEventListener("click",()=>{
+    definitionBtn.addEventListener("click",()=>{
         definitionSlider.classList.add("slide");
     })
 }
@@ -428,3 +301,129 @@ function selectWord(arr,tg){
 
 
 
+// generate ID and select word of the day
+
+selectWord(words,targetWord)
+
+
+//EVENT LISTENERS
+window.addEventListener("DOMContentLoaded",()=>{
+    let items = getLocalStorage();
+    if(items.length>0){
+        refreshDisplay(items);
+    }
+    
+})
+
+
+
+container.addEventListener("click", e=>{
+  if(!e.target.closest("span")) return;
+
+  if(e.target.closest("span") && play){
+  const targetLetter = e.target.innerHTML;
+ inputLetters(targetLetter)
+
+    }
+
+});
+
+backspace.addEventListener("click",()=>{
+    turn[counter-1].innerHTML = "";
+    counter--
+})
+
+enter.addEventListener("click",()=>{
+    let copy = [...targetWord];
+    let answer=[];
+    
+    if(play && counter ===5){
+    turn.forEach((letter,index)=>{
+        answer.push(letter.innerHTML);
+        for(let i = 0; i<targetWord.length;i++){
+            if(letter.innerHTML === targetWord[i] && index === i){
+            letter.classList.remove("orange")
+            setTimeout(changeGreen,270*index,letter)
+            targetWord[i] = "."
+            return;
+           }
+
+           else if(letter.innerHTML === targetWord[i] && index !== i){
+           letter.classList.remove("wrong")
+           setTimeout(changeOrange,270*index,letter)
+           
+           
+           }
+
+        }
+        if(!letter.classList.contains("green") && !letter.classList.contains("orange")){
+            setTimeout(changeGrey,270*index,letter);
+        }
+
+    })
+    targetWord = copy;
+    addToLocalStorage(answer);
+    let checker = 0;
+    for(let i=0; i<targetWord.length;i++){
+        if(answer[i]===targetWord[i]){
+            checker++
+        }
+    }
+ 
+    if(checker ===5 || turn ===sixthTry){
+        setTimeout(result,2500);
+        shareBtn.innerHTML = `<a href="whatsapp://send?text=I got ${turnNo}/6 try Wordl Slang! @ https://tales-1.github.io/WordlSlangEdition/">Share Result</a>`;
+        shareCont.appendChild(definitionBtn)
+        
+        addEvtListner()
+        play = false;
+        
+    }
+    
+    turnNo++
+    counter = 0;
+}
+else{
+    showAlert("Not enough letters")}
+})
+
+
+closeBtn.addEventListener("click", ()=>{
+    youWin.classList.remove("show")
+})
+
+shareBtn.addEventListener("click",()=>{
+    updateClipboard(`
+    I got ${turnNo}/6, try Wordl!`
+    );
+    showAlert("Copied to clipboard!")
+})
+
+cog.addEventListener("click",()=>{
+    result()
+    console.log(play)
+    if(play===false){
+        shareBtn.innerHTML =`<a href="whatsapp://send?text=I got ${turnNo}/6 try Wordl Slang! @ https://tales-1.github.io/WordlSlangEdition/">Share Result</a>`;
+        shareCont.appendChild(definitionBtn)
+        addEvtListner()
+
+    }
+
+})
+
+definitionSlider.addEventListener("click",()=>{
+    definitionSlider.classList.remove("slide")
+})
+
+debug.addEventListener("click",()=>{
+    removeFromLocalStorage();
+    window.location = window.location.href;
+})
+
+
+
+
+let definitionContent = "";
+definitionContent = targetWord.reduce((a,b)=>a+b);
+let insertDefinition = document.querySelector(".insert-definition");
+insertDefinition.innerHTML= definitionContent;
