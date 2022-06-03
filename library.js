d6f4267de9f27e718a9e10c501f2714c588df1de
Tales-1 <84051594+Tales-1@users.noreply.github.com>
2022-06-03 10:10:48 +0100

    Update library.js

--- library.js
+++ library.js
@@ -1,6 +1,6 @@
 export const words = [
     {
-        month:1,
+        month:5,
         date:16,
         definition:`<p>Middle aged woman, typically blonde, makes solutions to others' problems an inconvenience to her although she isn't even remotely affected.</p>
         <p class="example"> E.g. Karen complains that her favorite parking spot was replaced by a ramp for wheel chairs. She parks her car in the old spot anyway and shoots a vlog about it.</p>
@@ -8,7 +8,7 @@ export const words = [
         word: ["K","A","R","E","N"],
     },
     {
-        month:1,
+        month:5,
         date:17,
         definition:`<p>A popular, successful, but highly independent and self-reliant person. Typically a male.
         One step above an alpha male.</p>
@@ -16,7 +16,7 @@ export const words = [
         word:["S","I","G","M","A"],
     },
     {
-        month:1,
+        month:5,
         date:18,
         definition:`<p>Someone who does way too much for a person they like.</p>
         <p class="example">Brian shu the simp: I bought her a car and a whole ass house and we not even dating!
@@ -24,21 +24,21 @@ export const words = [
         word: ["S","I","M","P","S"],
     },
     {
-        month:1,
+        month:5,
         date:19,
         definition:`<p>Having no money. To be broke.</p>
         <p class="example">Sorry mate, I can't come out - I'm skint!</p>`,
         word:["S","K","I","N","T"],
     },
     {
-        month:1,
+        month:5,
         date:20,
         definition:`<p>rubbish, no good, bag of shite</p>
         <p class="example">you are pants</p>`,
         word:["P","A","N","T","S"],
     },
     {
-        month:1,
+        month:5,
         date:21,
         definition:`<p>isn't it</p>
         <p class="example">its just short for innit, innit? -- Ali G</p>`,
@@ -46,14 +46,14 @@ export const words = [
     },
     
     {
-        month:1,
+        month:5,
         date:22,
         definition:`<p>The use of the phrase "no cap" is meant to convey authenticity and truth. The phrase originated in reference to decorative gold teeth, which can be divided into two distinct varieties: permanent gold teeth (aka "perms") or caps (aka "pullouts"). Whereas caps can be pulled out with ease, perms, as their name suggests, are permanent. They cannot be taken out for a job interview or court date. They are an honest and lasting expression of the owners' realness.</p>
         <p class="example">I would only get perms because people who wear pullouts are fake, no cap.</p>`,
         word:["N","O","C","A","P"],
     },
     {
-        month:1,
+        month:5,
         date:23,
         definition:`<p> slang abbreviation for "natural". Usually used to refer to Natural Light beer. Also used in the health & fitness community to refer to bodybuilders who do not utilize performance-enhancing drugs, such as anabolic steroids.</p>
         <p class="example">1) Yo, someone toss me a natty from the fridge.</p>
@@ -62,14 +62,14 @@ export const words = [
         word: ["N","A","T","T","Y"],
     },
     {
-        month:1,
+        month:5,
         date:24,
         definition:`<p>Crew, posse, gang: an informal group of individuals with a common identity and a sense of solidarity. The term is a bit flashy and is more likely to be heard in hip-hop lyrics than in spoken conversation.</p>
         <p class="example">You step to me, and my whole squad will come down on you.</p>`,
         word:["S","Q","U","A","D"],
     },
     {
-        month:1,
+        month:5,
         date:25,
         definition:`<p>being very close friends with someone</p>
         <p class="example">Sanika: Wow, we are so tight Anushka! We are basically best friends!
@@ -78,7 +78,7 @@ export const words = [
     },
     
     {
-        month:1,
+        month:5,
         date:26,
         definition:`<p>Used to describe someone devoid of defining characteristics that might make a person interesting, extraordinary, or just simply worth devoting time or attention to.</p>`,
         word: ["B","A","S","I","C"],
@@ -91,29 +91,29 @@ export const words = [
         word:["G","H","O","S","T"],
     },
     {
-        month:1,
+        month:5,
         date:28,
         definition:`<p>Being salty is when you are upset over something little.</p>
         <p class="example">He was so salty after he died in smash bros</p>`,
         word:["S","A","L","T","Y"],
     },
     {
-        month:2,
-        date:1,
+        month:5,
+        date:29,
         definition:`<p>A light comment with slight disrespect towards an individual</p>
         <p class="example">Rose threw shade at Mona when she said there isn't a guy in her degree program Mona hadn't kissed.</p>`,
         word:["S","H","A","D","E"],
     },
     {
-        month:2,
-        date:2,
+        month:5,
+        date:30,
         definition:`<p>adj. scared, afraid, hesitant.</p>
         <p class="example">"They scared to death, they scared to look, they shook... cause ain't no such thing as halfway crooks." Mobb Deep, Shook Ones</p>`,
         word:["S","H","O","O","K"],
     },
     {
-        month:2,
-        date:3,
+        month:6,
+        date:1,
         definition:`<p>When you are being over the top, excessive, dramatic, and it's mostly over nothing!
         You are doing way too much if someone says you being extra.</p>
         <p class="example">Person A: "Yo ass is being too extra over a bag of hot fritos."
@@ -121,8 +121,8 @@ export const words = [
         word:["E","X","T","R","A"],
     },
     {
-        month:2,
-        date:4,
+        month:6,
+        date:2,
         definition:`<p>Greatest
         Of
         All
@@ -133,22 +133,22 @@ export const words = [
         word:["G","O","A","T","S"],
     },
     {
-        month:2,
-        date:5,
+        month:6,
+        date:3,
         definition:``,
         word:["L","I","N","G","O"],
     },
     {
-        month:2,
-        date:6,
+        month:6,
+        date:4,
         definition:`<p>A versatile slang term based on the luxury fashion brand meaning okay/good/great/awesome/fresh/etc. Originally used in the streets but now popularized by the masses. Sometimes abbreviated "gooch" if you're an expert in its use. To be all Gucci is a wonderful thing indeed.</p>
         <p class="example">"Hey. How are you? Haven't seen you in a while. Everything okay?"
         "Yeah everything's Gucci. I've just been busy."</p>`, 
         word:["G","U","C","C","I"],
     },
     {
-        month:2,
-        date:7,
+        month:6,
+        date:5,
         definition:`<p>To ignore, avoid or sidestep someone's obvious expression of interest through flirting or any means of advance.</p>
         <p class="example">Person 1: "Hey baby I miss you"
         
@@ -159,16 +159,16 @@ export const words = [
     },
 
     {
-        month:2,
-        date:8,
+        month:6,
+        date:6,
         definition:`<p>Usually said by the British when they see a joke-
         back when the law was made in Britain that allows citizens to criticize the king and queen, they had to remind themselves of whats a joke and what's not. Thus the creation of the sentence "That's Jokes" is made</p>.
         <p class="example">Hahahahha "That's Jokes" man.</p>`,
         word:["J","O","K","E","S"],
     },
     {
-        month:2,
-        date:9,
+        month:6,
+        date:7,
         definition:`<p>usually used when someone says or does something rididcoulous and it is as if to say ok whatever or you keep on doing that you fool</p>
         <p class="example">black guy: i get all the white girls
         white girl: skeen</p>`,
