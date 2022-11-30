7c9769bdc51277f949f78394dcc0f6e23c06745b
Tales-1 <j.naz248@outlook.com>
2022-11-30 11:35:10 +0000

    Update library.js

--- library.js
+++ library.js
@@ -1,180 +1,156 @@
 export const words = [
     {
-        month:5,
-        date:3,
-        definition:`<p>Middle aged woman, typically blonde, makes solutions to others' problems an inconvenience to her although she isn't even remotely affected.</p>
+         definition:`<p>Middle aged woman, typically blonde, makes solutions to others' problems an inconvenience to her although she isn't even remotely affected.</p>
         <p class="example"> E.g. Karen complains that her favorite parking spot was replaced by a ramp for wheel chairs. She parks her car in the old spot anyway and shoots a vlog about it.</p>
         `,
         word: ["K","A","R","E","N"],
+        id:1,
+        
     },
+
     {
-        month:5,
-        date:17,
         definition:`<p>A popular, successful, but highly independent and self-reliant person. Typically a male.
         One step above an alpha male.</p>
         <p class="example">E.g: Unlike the simps of twitch, David was a sigma male as evidenced by his work ethic and the women throwing themselves at him.</p>`,
         word:["S","I","G","M","A"],
+        id:2,
     },
+
     {
-        month:5,
-        date:18,
         definition:`<p>Someone who does way too much for a person they like.</p>
         <p class="example">Brian shu the simp: I bought her a car and a whole ass house and we not even dating!
         Nathan the pimp: U a simp.</p>`,
         word: ["S","I","M","P","S"],
+        id:3,
     },
+
     {
-        month:5,
-        date:19,
         definition:`<p>Having no money. To be broke.</p>
         <p class="example">Sorry mate, I can't come out - I'm skint!</p>`,
         word:["S","K","I","N","T"],
+        id:4,
     },
+
     {
-        month:5,
-        date:20,
         definition:`<p>rubbish, no good, bag of shite</p>
         <p class="example">you are pants</p>`,
         word:["P","A","N","T","S"],
+        id:5,
     },
+
     {
-        month:5,
-        date:21,
         definition:`<p>isn't it</p>
         <p class="example">its just short for innit, innit? -- Ali G</p>`,
         word:["I","N","N","I","T"],
+        id:6,
     },
     
     {
-        month:5,
-        date:22,
         definition:`<p>The use of the phrase "no cap" is meant to convey authenticity and truth. The phrase originated in reference to decorative gold teeth, which can be divided into two distinct varieties: permanent gold teeth (aka "perms") or caps (aka "pullouts"). Whereas caps can be pulled out with ease, perms, as their name suggests, are permanent. They cannot be taken out for a job interview or court date. They are an honest and lasting expression of the owners' realness.</p>
         <p class="example">I would only get perms because people who wear pullouts are fake, no cap.</p>`,
         word:["N","O","C","A","P"],
+        id:7,
     },
+
     {
-        month:5,
-        date:23,
         definition:`<p> slang abbreviation for "natural". Usually used to refer to Natural Light beer. Also used in the health & fitness community to refer to bodybuilders who do not utilize performance-enhancing drugs, such as anabolic steroids.</p>
         <p class="example">1) Yo, someone toss me a natty from the fridge.</p>
         
         <p class="example">2) I don't care how big you can get on roids...I'd rather be natty and 190 lbs., than 250 lbs. and juicing.</p>`,
         word: ["N","A","T","T","Y"],
+        id:8,
     },
     {
-        month:5,
-        date:24,
         definition:`<p>Crew, posse, gang: an informal group of individuals with a common identity and a sense of solidarity. The term is a bit flashy and is more likely to be heard in hip-hop lyrics than in spoken conversation.</p>
         <p class="example">You step to me, and my whole squad will come down on you.</p>`,
         word:["S","Q","U","A","D"],
+        id:9,
     },
-    {
-        month:5,
-        date:25,
+    { 
         definition:`<p>being very close friends with someone</p>
         <p class="example">Sanika: Wow, we are so tight Anushka! We are basically best friends!
         Anushka: Ikr,we are so tight, Sanika!</p>`,
         word:["T","I","G","H","T"],
+        id:10,
     },
     
     {
-        month:5,
-        date:26,
         definition:`<p>Used to describe someone devoid of defining characteristics that might make a person interesting, extraordinary, or just simply worth devoting time or attention to.</p>`,
         word: ["B","A","S","I","C"],
+        id:11,
     },
     {
-        month:1,
-        date:27,
         definition:`<p>To avoid someone until they get the picture and stop contacting you.</p>
         T<p class="example">his mother fucker is annoying yo. I'm guna have to ghost him until he gets the point.</p>`,
         word:["G","H","O","S","T"],
+        id:12,
     },
     {
-        month:5,
-        date:28,
         definition:`<p>Being salty is when you are upset over something little.</p>
         <p class="example">He was so salty after he died in smash bros</p>`,
         word:["S","A","L","T","Y"],
+        id:13,
     },
     {
-        month:5,
-        date:29,
         definition:`<p>A light comment with slight disrespect towards an individual</p>
         <p class="example">Rose threw shade at Mona when she said there isn't a guy in her degree program Mona hadn't kissed.</p>`,
         word:["S","H","A","D","E"],
+        id:14,
     },
     {
-        month:5,
-        date:30,
         definition:`<p>adj. scared, afraid, hesitant.</p>
         <p class="example">"They scared to death, they scared to look, they shook... cause ain't no such thing as halfway crooks." Mobb Deep, Shook Ones</p>`,
         word:["S","H","O","O","K"],
+        id:15,
     },
     {
-        month:6,
-        date:1,
-        definition:`<p>When you are being over the top, excessive, dramatic, and it's mostly over nothing!
+         definition:`<p>When you are being over the top, excessive, dramatic, and it's mostly over nothing!
         You are doing way too much if someone says you being extra.</p>
         <p class="example">Person A: "Yo ass is being too extra over a bag of hot fritos."
         Person B: "Bitch, just don't touch my food."</p>`,
         word:["E","X","T","R","A"],
+        id:16,
     },
     {
-        month:6,
-        date:2,
-        definition:`<p>Greatest
-        Of
-        All
-        Time</p>
-        
-        
-        <p class="example">Those shoes are G.o.a.t.</p>`,     
+         definition:`<p>Greatest Of All Time</p>
+            <p class="example">Those shoes are G.o.a.t.</p>`,     
         word:["G","O","A","T","S"],
+        id:17,
     },
     {
-        month:6,
-        date:3,
         definition:``,
         word:["L","I","N","G","O"],
+        id:18,
     },
     {
-        month:6,
-        date:4,
-        definition:`<p>A versatile slang term based on the luxury fashion brand meaning okay/good/great/awesome/fresh/etc. Originally used in the streets but now popularized by the masses. Sometimes abbreviated "gooch" if you're an expert in its use. To be all Gucci is a wonderful thing indeed.</p>
+         definition:`<p>A versatile slang term based on the luxury fashion brand meaning okay/good/great/awesome/fresh/etc. Originally used in the streets but now popularized by the masses. Sometimes abbreviated "gooch" if you're an expert in its use. To be all Gucci is a wonderful thing indeed.</p>
         <p class="example">"Hey. How are you? Haven't seen you in a while. Everything okay?"
         "Yeah everything's Gucci. I've just been busy."</p>`, 
         word:["G","U","C","C","I"],
+        id:19,
     },
     {
-        month:6,
-        date:5,
-        definition:`<p>To ignore, avoid or sidestep someone's obvious expression of interest through flirting or any means of advance.</p>
+         definition:`<p>To ignore, avoid or sidestep someone's obvious expression of interest through flirting or any means of advance.</p>
         <p class="example">Person 1: "Hey baby I miss you"
-        
         Person 2: "Goodnight bruh"
-        
         #curve</p>`,
         word:["C","U","R","V","E"],
+        id:20
     },
-
     {
-        month:6,
-        date:6,
         definition:`<p>Usually said by the British when they see a joke-
         back when the law was made in Britain that allows citizens to criticize the king and queen, they had to remind themselves of whats a joke and what's not. Thus the creation of the sentence "That's Jokes" is made</p>.
         <p class="example">Hahahahha "That's Jokes" man.</p>`,
         word:["J","O","K","E","S"],
+        id:21,
     },
     {
-        month:6,
-        date:7,
         definition:`<p>usually used when someone says or does something rididcoulous and it is as if to say ok whatever or you keep on doing that you fool</p>
         <p class="example">black guy: i get all the white girls
         white girl: skeen</p>`,
         word:["S","K","E","E","N"],
+        id:22,
     },
-
 ]
 
 
